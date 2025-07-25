0e92feb932d67ccc32cb4d6c94c82c0aa3a445cb
itsnami <barbarianmadness20@gmail.com>
2025-07-25 12:28:58 -0500

    madlibs game

---
+++ madlibsgame.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    // mad libs game
+
+    char noun[50] = "";
+    char verb[50] = "";
+    char adjective1[50] = "";
+    char adjective2[50] = "";
+    char adjective3[50] = "";
+
+    printf("enter adjective(description): ");
+    fgets(adjective1, sizeof(adjective1), stdin);
+
+    printf("enter noun(animal or person): ");
+    
+    printf("enter adjective(description): ");
+    
+    printf("enter verb(ending with - ing): ");
+
+    return 0;
+
+}
